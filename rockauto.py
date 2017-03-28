99deaf811929232eb058791bd1c7b10ee4959832
frankcreating <412632756@qq.com>
2017-03-28 08:00:02 +0000

    Update rockauto.py
    fixed some bug and now it can work for searching car parts' oe.Next time will work for save picture on local file.

--- rockauto.py
+++ rockauto.py
@@ -11,15 +11,17 @@ class swamp(object):
 	partnum:123
 	'''
 	url='http://www.rockauto.com/en/partsearch/?'
-	def __init__(self,partnum,oe_num):
+	def __init__(self):
+		partnum,oe_num=door.usr_wx()
 		self.partnum=partnum
 		self.oe_num=oe_num
 		print 'start!'
 		req=self.get_html()
-		print req
 		text=req.content
 		self.text_save(partnum,text)
-		print 'finish!'
+		finaltext=Search_method.bs(partnum)#use search method to find what we want 
+		print finaltext
+		print 'finish!and results already saved in local address%s'%partnum
 	def get_html(self):
 		print 'now loading Please wait for a moment!'
 		url=self.url+'partnum='+self.oe_num
@@ -33,34 +35,44 @@ class swamp(object):
  
 	def pic_save():
 		pass
-	def text_save(self,name,text):#save $text(which you wanto save) as local text
+	def text_save(self,name,text):#save $text(which you wanto save) as local text,name ==partnum
 		with open('/home/frank/%s.txt'%name,'w+') as f:
 			f.write(text)
 
 	def text_read(self):#read text which save just a moment
 		f=open('/home/frank/test.txt','r')
-		tx=f.read()
+		tx=f.read()#return tx as final result
 		if lambda:f is not None or f !='':
 			print 'Yeah,we got something'
 			f.close()
 			return tx
 		else:
+			print 'it is totally empty!'
 			f.close()
 	def sql(self):
 		x='123'
 		return x
-	def usr_wx():
-		print 'continue for searching oe?y/n?'
-		a=input()
+
+
+class door():
+	@staticmethod
+	def usr_wx():#user operation interfaces
+
+		print 'ready for searching oe?y/n?'
+		a=raw_input()
 		if a =='y' or a== 'yes':
 			print 'what is file name you want to save?partnum :'
-			partnum=input()
+			partnum=raw_input()
 			print 'input oe_num:'
-			oe_num=input()
+			oe_num=raw_input()
+			return partnum,oe_num
 
-
+		else:
+			print 'USER EXIE!PROGRAM IS DONE!'
+			return 0,0
 class Search_method(object):#several ways to get detailed information
-	def bs(text_name):
+	@classmethod
+	def bs(self,text_name):
 		soup=BeautifulSoup(open('/home/frank/%s.txt'%text_name),'html5lib')#text_name == partnum Beacuse I save text's name as partnum
 		res=soup.findAll('span',{"title":"Replaces these Alternate/ OE Part Numbers"})
 		return res
@@ -71,4 +83,4 @@ class Search_method(object):#several ways to get detailed information
 
 
 if __name__=="__main__":
-	a=swamp('fctext','0905038')
+	a=swamp()#'fctext','0905038'
